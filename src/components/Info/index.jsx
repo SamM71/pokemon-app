06224d0f89de41568cdb790c012ac73fef8e54a4
SamM71 <sammocock@gmail.com>
2023-08-30 18:26:01 +0100

    stop crashing bug

--- src/components/Info/index.jsx
+++ src/components/Info/index.jsx
@@ -1,13 +1,42 @@
+import { useEffect } from "react"
+
 function Info(props) {
+  let name, id, height, weight = ''
+
+
+  async function getInfo() {
+    try {
+      console.log(props)
+      if (props.pokeData.species) {
+        name = props.pokeData.species.name
+        console.log(name)
+        id = props.pokeData.id
+        height = props.pokeData.height
+        weight = props.pokeData.weight
+        return [name, id, height, weight]
+      }
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+
+  useEffect(() => {
+    getInfo()
+  }, [getInfo()])
+
   return (
     <>
       <div>
         <ul>
           <li>
-            {props.pokeData.id}
+            Name: {name}
           </li>
           <li>
-            {props.pokeData.weight}
+            Number: {id}
+          </li>
+          <li>
+            Weight: {weight}
           </li>
         </ul>
       </div>
